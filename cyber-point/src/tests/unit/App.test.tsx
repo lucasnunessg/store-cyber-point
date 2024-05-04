9c8a13201c175e4ec6421f43b73e2e8b86f3a4f5
LucasNunesDEV <lucasnunespacheco@gmail.com>
2024-05-04 17:12:05 -0300

    implementando click em botões, mas ainda sem sucesso

--- cyber-point/src/tests/unit/App.test.tsx
+++ cyber-point/src/tests/unit/App.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { stub } from 'sinon';
 
 import Api from '../../components/api'
 import MyHeader from '../../components/MyHeader';
@@ -19,3 +20,21 @@ render(<Api />)
 const inputPreviousPage = screen.getByText(/Anterior/i);
 expect(inputPreviousPage).toBeInTheDocument();
 });
+
+describe('Testes dos botões de paginação', () => {
+    it('deve chamar a função handleNextPage quando o botão "Próximo" é clicado', () => {
+      const handleNextPage = stub();
+      const { getByText } = render(<Api handleNextPage={handleNextPage} />);
+      const btnNext = getByText(/Próximo/i);
+      fireEvent.click(btnNext);
+      expect(handleNextPage.calledOnce).toBeTruthy();
+    });
+    
+    it('deve chamar a função handlePrevPage quando o botão "Anterior" é clicado', () => {
+      const handlePrevPage = stub();
+      const { getByText } = render(<Api handlePrevPage={handlePrevPage} />);
+      const btnPrev = getByText(/Anterior/i);
+      fireEvent.click(btnPrev);
+      expect(handlePrevPage.calledOnce).toBeTruthy();
+    });
+  });
