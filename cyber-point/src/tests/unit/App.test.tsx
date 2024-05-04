76a35ff8c98dc8ed9347162c19f1ac7ff7d8adb2
LucasNunesDEV <lucasnunespacheco@gmail.com>
2024-05-03 21:32:36 -0300

    mais um teste

--- cyber-point/src/tests/unit/App.test.tsx
+++ cyber-point/src/tests/unit/App.test.tsx
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 
+import Api from '../../components/api'
 import MyHeader from '../../components/MyHeader';
 
 it('Testa se o header é "Lucas Store', () => {
     render (<MyHeader/>);
     expect(screen.getByText(/lucas store/i)).toBeInTheDocument();
+});
+
+it('Testa se acha o botão de "Próximo"', () => {
+    render (<Api />)
+    const inputNextPage = screen.getByText('Próximo');
+    expect(inputNextPage).toBeInTheDocument();
 });
