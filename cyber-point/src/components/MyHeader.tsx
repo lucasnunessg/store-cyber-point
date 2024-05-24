040cf15cf9bfe4355d9324f9515806c53e28d4a0
lucasnunessg <lucasnunespacheco@gmail.com>
2024-05-24 16:36:35 -0300

    criando área de login mas sem faze-la funcionar ainda

--- cyber-point/src/components/MyHeader.tsx
+++ cyber-point/src/components/MyHeader.tsx
@@ -1,8 +1,11 @@
+import Login from './login';
+
 function MyHeader (){
   return (
 <div className="myHeader">
   <header>
     <h1> Lucas Store </h1>
+    <Login />
     <hr></hr>
   </header>
 </div>
---
+++ cyber-point/src/components/login.tsx
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import { FormEvent, useState } from 'react';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('/login', {
+        email,
+        password,
+      });
+      console.log('Usuário logado', response.data);
+    } catch (error) {
+      setError('Credenciais inválidas, por favor, tente novamente');
+      console.log('erro ao logar', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {error && <div>{error}</div>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='email'>E-mail:</label>
+          <input
+            type='text'
+            id='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
