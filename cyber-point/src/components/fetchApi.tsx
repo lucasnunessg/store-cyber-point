a9cbf107c8fcd1464d22bf7105002625af2f5e26
lucasnunessg <lucasnunespacheco@gmail.com>
2024-06-05 19:09:43 -0300

    começando logica de success login

--- cyber-point/src/components/fetchApi.tsx
+++ cyber-point/src/components/fetchApi.tsx
@@ -1,7 +1,26 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
 const api = axios.create({
   baseURL: 'http://localhost:3001', 
 });
+
+interface LoginResponse {
+  token: string;
+}
+
+const login = async (email: string, password: string): Promise<void> => {
+  try {
+    const response: AxiosResponse<LoginResponse> = await api.post('/login', { email, password });
+    const token: string = response.data.token;
+    localStorage.setItem('token', token);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const axiosError: AxiosError = error;
+      console.error('Erro de autenticação:', axiosError.message);
+    } else {
+      console.error('Erro desconhecido:', error);
+    }
+  }
+};
 
 export default api;
