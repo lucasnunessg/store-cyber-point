75db06c81bcba194567358935cb31daf698f3f3d
lucasnunessg <lucasnunespacheco@gmail.com>
2024-06-20 16:26:03 -0300

    adicionando browser router ao main.tsx

--- cyber-point/src/main.tsx
+++ cyber-point/src/main.tsx
@@ -1,35 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import MyHeader from './components/MyHeader.tsx';
-import Login from './components/login.tsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx'
+import MyHeader from './components/MyHeader.tsx'
+import './index.css'
 
-const Root = () => {
-  const token = localStorage.getItem('token');
-
-  const PrivateRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    return token ? <>{children}</> : <Navigate to="/login" />;
-  };
-
-  return (
-    <Router>
-      <MyHeader />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/products" element={<PrivateRoute><ProductManagement /></PrivateRoute>} />
-        <Route path="/" element={<PrivateRoute><App /></PrivateRoute>} />
-      </Routes>
-    </Router>
-  );
-};
-
-const domNode = document.getElementById('root')!;
-const root = createRoot(domNode);
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Root />
-  </React.StrictMode>
-);
+    <BrowserRouter>
+    <MyHeader />
+    <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
