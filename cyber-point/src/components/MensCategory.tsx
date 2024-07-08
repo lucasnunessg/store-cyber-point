465d062c1ad1be80333d55ae9d1e28b6e364a96c
lucasnunessg <lucasnunespacheco@gmail.com>
2024-07-08 16:07:36 -0300

    paginação feita

--- cyber-point/src/components/MensCategory.tsx
+++ cyber-point/src/components/MensCategory.tsx
@@ -6,6 +6,10 @@ const MensCategory = () => {
   const [allProducts, setAllProducts] = useState<Product[]>([]);
   const [filteredProducts, setFilteredProducts] = useState<Product[]>([]); 
   const [search, setSearch] = useState<string>(''); 
+  const [currentPage, setCurrentPage] = useState<number>(1); 
+  const [totalPages, setTotalPages] = useState<number>(1); 
+  const productsPerPage = 4;
+
 
 
 
@@ -24,14 +28,25 @@ const MensCategory = () => {
   }, []);
 
   useEffect(() => {
-    const filtered = allProducts.filter((product) => 
+    const filtered = allProducts.filter((product) => //faz um filtro no novo array e verifica a condiçao, se contem, devolve true
     product.title.toLowerCase().includes(search.toLowerCase()));
-    setFilteredProducts(filtered)
-  }, [search, allProducts])
+    setTotalPages(filtered.length / productsPerPage)
+
+    const startIndex = (currentPage - 1) * productsPerPage;
+    const endIndex = startIndex + productsPerPage;
+    setFilteredProducts(filtered.slice(startIndex, endIndex));
+  }, [search, allProducts, currentPage])
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value)
   };
+
+  const goToNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  }
+  const goToPrevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  }
 
 
   return (
@@ -45,6 +60,11 @@ const MensCategory = () => {
     value={search}
     onChange={handleSearch} 
     />
+    <div className='pagination'>
+      <button onClick={goToPrevPage} disabled={currentPage === 1}>Anterior</button>
+      <span>Página: {currentPage} de {totalPages}</span>
+      <button onClick={goToNextPage} disabled={currentPage === totalPages}>Próximo</button>
+    </div>
       </div>
       {filteredProducts.map((product) => (
         <div key={product.id} className='product-item'>
