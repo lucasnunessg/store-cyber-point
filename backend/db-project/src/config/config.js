ccac47cd7425fc2719aa1864b827070fdaf5aa21
lucasnunessg <lucasnunespacheco@gmail.com>
2024-07-15 16:36:05 -0300

    começando a utilizar o docker compose

--- backend/db-project/src/config/config.js
+++ backend/db-project/src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
-  host: process.env.MYSQL_HOST,
+  username: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
+  database: process.env.MYSQL_DATABASE || 'db_store_cyber',
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: process.env.MYSQL_PORT || 3306,
   dialect: 'mysql',
 };
 
