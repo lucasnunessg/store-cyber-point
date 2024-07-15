304e24e3c626ed04c5a5c3a64aa3f9c1c21cf9b4
lucasnunessg <lucasnunespacheco@gmail.com>
2024-07-15 15:25:54 -0300

    criando tabela para cometnarios

---
+++ backend/db-project/src/migrations/20240715182119-create-comments.js
@@ -0,0 +1,51 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      comment: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      productId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'product_id',
+        references: {
+          model: 'products',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      clientId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'client_id',
+        references: {
+          model: 'clients',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('comments');
+  }
+};
