4359f9efba524fca2b4b22fa116f1ca45c10b9e6
lucasnunessg <lucasnunespacheco@gmail.com>
2024-07-16 15:53:12 -0300

    dando uma pausa

--- cyber-point/src/components/Comments.tsx
+++ cyber-point/src/components/Comments.tsx
@@ -2,29 +2,55 @@ import { useState, useEffect } from 'react';
 import api from './fetchApi';
 
 const Comments = () => {
-  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState([]);
   const [newComment, setNewComment] = useState('');
+  const [clientId, setClientId] = useState('');
+  const [productId, setProductId] = useState('');
 
   useEffect(() => {
     async function fetchComments() {
       try {
-        const response = await api.get(`/products/${productId}/comments`);
-        setComments(response.data);
+        const response = await api.get(`/products/:${productId}/comments`);
+        setComment(response.data);
       } catch (error) {
         console.error('Não foi possível encontrar o comentário', error);
       }
     }
     fetchComments();
-  }, []);
+  }, [productId]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    
+ try{
+  const newComment = {
+    clientId,
+    productId,
+    comment,
   }
+  const comentPost = await api.post('/products/:id/comments', newComment);
+    setNewComment(comentPost.data)
+    console.log("Comentário criado com sucesso!", comentPost.data);
+    setClientId('');
+    setProductId('');
+ }   catch(e){
+  console.error('erro ao buscar comentário', e)
+ }
+ 
+}
 
   return (
-    <div>
-      {/* Render comments here */}
-    </div>
+    <form onSubmit={handleSubmit}>
+      <div>
+      <h4>Comentários do produto:</h4>
+      <input
+      type='text'
+      value={newComment}
+      onChange={(e) => setNewComment(e.target.value)}
+      />
+      </div>
+    </form>
+    
   );
 };
 
