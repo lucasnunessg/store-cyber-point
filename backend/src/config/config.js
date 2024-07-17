4a3d840909a05c81944a255874f4181ee6481a4f
lucasnunessg <lucasnunespacheco@gmail.com>
2024-07-17 15:09:15 -0300

    mudei o host de "db" pra "localhost" para conseguir executar querys fora do container

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,7 +2,7 @@ const config = {
   username: process.env.DB_USER || 'root',
   password: process.env.MYSQL_ROOT_PASSWORD || 'password',
   database: process.env.MYSQL_DATABASE || 'db_store_cyber',
-  host: process.env.DB_HOST || 'db',
+  host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 3307,
   dialect: 'mysql',
 };
