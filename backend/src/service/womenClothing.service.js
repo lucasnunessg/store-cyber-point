5fdb51939cedb4e3346b326361e125a43ab90876
lucasnunessg <lucasnunespacheco@gmail.com>
2024-07-17 16:37:16 -0300

    agora sim corrigindo tudo

--- backend/src/service/womenClothing.service.js
+++ backend/src/service/womenClothing.service.js
@@ -2,7 +2,7 @@ const { Product } = require('../models')
 
 const getWomenCloth = async () => {
 
-  const products = await Product.findAll({ where: { category: 'women\'s clothing' } }) // precisa ter women\'s pro Sequelize conseguir ler a aspa
+  const products = await Product.findAll({ where: { category: 'women\'s clothing' } }) 
   
   return products;
   
