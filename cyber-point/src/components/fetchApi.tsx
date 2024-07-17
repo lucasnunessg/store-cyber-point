5a442fd29332e23e2784382b11fb09f675551788
lucasnunessg <lucasnunespacheco@gmail.com>
2024-07-17 16:36:59 -0300

    agora sim

--- cyber-point/src/components/fetchApi.tsx
+++ cyber-point/src/components/fetchApi.tsx
@@ -2,11 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://localhost:3001',
-  // comentado pq quebrou aplicação withCredentials: true,
 
 });
-
-
 
 
 export default api;
