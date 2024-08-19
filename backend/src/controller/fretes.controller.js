98b825f6e76f7c6129aeb059a2838973a76d2391
lucasnunessg <lucasnunespacheco@gmail.com>
2024-08-19 10:01:11 -0300

    fazendo verificacoes na controller

--- backend/src/controller/fretes.controller.js
+++ backend/src/controller/fretes.controller.js
@@ -3,12 +3,18 @@ const { freteService } = require('../service');
 const valorFrete = async (req, res) => {
   const { price, category } = req.body;
 
+  const priceNumber = parseFloat(price);
+
+  if(isNaN(priceNumber)) {
+    return res.status(400).json({ message: 'O preço deve ser um número válido.' })
+  }
+
   try {
     const productsPerCategory = await freteService.getProductPerCategory(category);
     const productsPerPrice = await freteService.getProductsPerPrice(price);
 
     const fretePreco = freteService.calcularFrete(productsPerCategory, productsPerPrice);
-    if (fretePreco === undefined) {
+    if (fretePreco === undefined || isNaN(fretePreco)) {
       return res.status(404).json({ message: 'Não foi possível calcular o frete' });
     }
 
